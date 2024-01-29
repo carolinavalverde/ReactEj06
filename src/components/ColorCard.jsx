e5331791d10a3421cb0069774e1ba6a97505f04f
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-29 16:12:49 -0300

    agrego mas estilos

--- src/components/ColorCard.jsx
+++ src/components/ColorCard.jsx
@@ -5,7 +5,7 @@ const ColorCard = ({ color, deleteColor }) => {
     <div className="card">
       <div className="card-body">
         <div style={{ backgroundColor: color, height: "100px" }}></div>
-        <p className="card-text">{color}</p>
+        <p className="card-text text-uppercase text-center fw-bold">{color}</p>
         <button onClick={() => deleteColor(color)} className="btn btn-danger">
           Borrar
         </button>
